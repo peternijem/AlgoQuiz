0b430aebd9c99d7f32c3a827339a78608064b74b
pnijem <42067687+pnijem@users.noreply.github.com>
2018-08-28 22:02:01 +0300

    Update ReverseLinkedList.java

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -1,56 +1,122 @@
+/**
+Given a singly linked list, rotate the linked list counter-clockwise by k nodes. Where k is a given positive integer. 
+For example, if the given linked list is 10->20->30->40->50->60 and k is 4, the list should be modified to 50->60->10->20->30->40. 
+Assume that k is smaller than the count of nodes in linked list.
 
-//Java program for reversing the linked list
+ANSWER:
+To rotate the linked list, we need to change next of kth node to NULL, next of last node to previous head node, and finally change head to (k+1)th node. 
+So we need to get hold of three nodes: kth node, (k+1)th node and last node.
+Traverse the list from beginning and stop at kth node. Store pointer to kth node. We can get (k+1)th node using kthNode->next.
+Keep traversing till end and store pointer to last node also. Finally, change pointers as stated above.
+**/
 
+
+// Java program to rotate a linked list
+ 
 class ReverseLinkedList {
-
- static Node head;
-
- static class Node {
-
-     int data;
-     Node next;
-
-     Node(int d) {
-         data = d;
-         next = null;
-     }
- }
-
- /* Function to reverse the linked list */
- Node reverse(Node node) {
-     Node prev = null;
-     Node current = node;
-     Node next = null;
-     while (current != null) {
-         next = current.next;
-         current.next = prev;
-         prev = current;
-         current = next;
-     }
-     node = prev;
-     return node;
- }
-
- // prints content of double linked list
- void printList(Node node) {
-     while (node != null) {
-         System.out.print(node.data + " ");
-         node = node.next;
-     }
- }
-
- public static void main(String[] args) {
-	 ReverseLinkedList list = new ReverseLinkedList();
-     list.head = new Node(85);
-     list.head.next = new Node(15);
-     list.head.next.next = new Node(4);
-     list.head.next.next.next = new Node(20);
-      
-     System.out.println("Given Linked list");
-     list.printList(head);
-     head = list.reverse(head);
-     System.out.println("");
-     System.out.println("Reversed linked list ");
-     list.printList(head);
- }
+    Node head;  // head of list
+ 
+    /* Linked list Node*/
+    class Node
+    {
+        int data;
+        Node next;
+        
+		Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+ 
+    // This function rotates a linked list counter-clockwise
+    // and updates the head. The function assumes that k is
+    // smaller than size of linked list. It doesn't modify
+    // the list if k is greater than or equal to size
+    void rotate(int k)
+    {
+        if (k == 0) return;
+ 
+        // Let us understand the below code for example k = 4
+        // and list = 10->20->30->40->50->60.
+        Node current  = head;
+ 
+        // current will either point to kth or NULL after this
+        // loop. current will point to node 40 in the above example
+        int count = 1;
+        while (count < k && current !=  null)
+        {
+            current = current.next;
+            count++;
+        }
+ 
+        // If current is NULL, k is greater than or equal to count
+        // of nodes in linked list. Don't change the list in this case
+        if (current == null)
+            return;
+ 
+        // current points to kth node. Store it in a variable.
+        // kthNode points to node 40 in the above example
+        Node kthNode = current;
+ 
+        // current will point to last node after this loop
+        // current will point to node 60 in the above example
+        while (current.next != null)
+            current = current.next;
+ 
+        // Change next of last node to previous head
+        // Next of 60 is now changed to node 10
+ 
+        current.next = head;
+ 
+        // Change head to (k+1)th node
+        // head is now changed to node 50
+        head = kthNode.next;
+ 
+        // change next of kth node to null
+        kthNode.next = null;
+ 
+    }
+ 
+    /*  Given a reference (pointer to pointer) to the head
+        of a list and an int, push a new node on the front
+        of the list. */
+    void push(int new_data) {
+        /* 1 & 2: Allocate the Node &
+                  Put in the data*/
+        Node new_node = new Node(new_data);
+ 
+        /* 3. Make next of new Node as head */
+        new_node.next = head;
+ 
+        /* 4. Move the head to point to new Node */
+        head = new_node;
+    }
+ 
+    void printList() {
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+ 
+    /* Drier program to test above functions */
+    public static void main(String args[]) {
+    	ReverseLinkedList llist = new ReverseLinkedList();
+ 
+        // create a list 10->20->30->40->50->60
+        for (int i = 60; i >= 10; i -= 10)
+            llist.push(i);
+ 
+        System.out.println("Given list");
+        llist.printList();
+ 
+        llist.rotate(4);
+ 
+        System.out.println("Rotated Linked List");
+        llist.printList();
+    }
 }
