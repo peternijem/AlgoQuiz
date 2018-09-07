af0bf4bae616bc393a752676ba8d5b8ffa1f825e
pnijem <42067687+pnijem@users.noreply.github.com>
2018-09-07 19:20:34 +0300

    Create AbstractFactoryPattern.java
    Java program to demonstrate Abstract Factory Design Pattern

---
+++ AbstractFactoryPattern.java
@@ -0,0 +1,194 @@
+
+/**
+ * Create a Shape and Color interfaces and concrete classes implementing these interfaces. 
+ * Create an abstract factory class AbstractFactory as next step. 
+ * Factory classes ShapeFactory and ColorFactory are defined where each factory extends AbstractFactory. 
+ * A factory creator/generator class FactoryProducer is created.
+ * The main method uses FactoryProducer to get a AbstractFactory object. 
+ * It will pass information (CIRCLE / RECTANGLE / SQUARE for Shape) to AbstractFactory to get the type of object it needs. 
+ * It also passes information (RED / GREEN / BLUE for Color) to AbstractFactory to get the type of object it needs.
+ * @author Peter
+ *
+ */
+public class AbstractFactoryPattern {
+
+	public interface Shape {
+		void draw();
+	}
+
+	public class Rectangle implements Shape {
+
+		@Override
+		public void draw() {
+			System.out.println("Inside Rectangle::draw() method.");
+		}
+	}
+
+	public class Square implements Shape {
+
+		@Override
+		public void draw() {
+			System.out.println("Inside Square::draw() method.");
+		}
+	}
+
+	public class Circle implements Shape {
+
+		@Override
+		public void draw() {
+			System.out.println("Inside Circle::draw() method.");
+		}
+	}
+
+	public interface Color {
+		void fill();
+	}
+
+	public class Red implements Color {
+
+		@Override
+		public void fill() {
+			System.out.println("Inside Red::fill() method.");
+		}
+	}
+
+	public class Green implements Color {
+
+		@Override
+		public void fill() {
+			System.out.println("Inside Green::fill() method.");
+		}
+	}
+
+	public class Blue implements Color {
+
+		@Override
+		public void fill() {
+			System.out.println("Inside Blue::fill() method.");
+		}
+	}
+
+	public abstract class AbstractFactory {
+		abstract Color getColor(String color);
+		abstract Shape getShape(String shape) ;
+	}
+
+	public class ShapeFactory extends AbstractFactory {
+
+		@Override
+		public Shape getShape(String shapeType){
+
+			if(shapeType == null){
+				return null;
+			}		
+
+			if(shapeType.equalsIgnoreCase("CIRCLE")){
+				return new Circle();
+
+			}else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+				return new Rectangle();
+
+			}else if(shapeType.equalsIgnoreCase("SQUARE")){
+				return new Square();
+			}
+
+			return null;
+		}
+
+		@Override
+		Color getColor(String color) {
+			return null;
+		}
+	}
+
+	public class ColorFactory extends AbstractFactory {
+
+		@Override
+		public Shape getShape(String shapeType){
+			return null;
+		}
+
+		@Override
+		Color getColor(String color) {
+
+			if(color == null){
+				return null;
+			}		
+
+			if(color.equalsIgnoreCase("RED")){
+				return new Red();
+
+			}else if(color.equalsIgnoreCase("GREEN")){
+				return new Green();
+
+			}else if(color.equalsIgnoreCase("BLUE")){
+				return new Blue();
+			}
+
+			return null;
+		}
+	}
+
+	public class FactoryProducer {
+		public AbstractFactory getFactory(String choice){
+
+			if(choice.equalsIgnoreCase("SHAPE")){
+				return new ShapeFactory();
+
+			}else if(choice.equalsIgnoreCase("COLOR")){
+				return new ColorFactory();
+			}
+
+			return null;
+		}
+	}
+
+
+	public static void main(String[] args) {
+		AbstractFactoryPattern abstractFactoryPattern = new AbstractFactoryPattern();
+		AbstractFactoryPattern.FactoryProducer factoryProducer = abstractFactoryPattern.new FactoryProducer();
+		
+		//get shape factory
+		AbstractFactory shapeFactory = factoryProducer.getFactory("SHAPE");
+
+		//get an object of Shape Circle
+		Shape shape1 = shapeFactory.getShape("CIRCLE");
+
+		//call draw method of Shape Circle
+		shape1.draw();
+
+		//get an object of Shape Rectangle
+		Shape shape2 = shapeFactory.getShape("RECTANGLE");
+
+		//call draw method of Shape Rectangle
+		shape2.draw();
+
+		//get an object of Shape Square 
+		Shape shape3 = shapeFactory.getShape("SQUARE");
+
+		//call draw method of Shape Square
+		shape3.draw();
+
+		//get color factory
+		AbstractFactory colorFactory = factoryProducer.getFactory("COLOR");
+
+		//get an object of Color Red
+		Color color1 = colorFactory.getColor("RED");
+
+		//call fill method of Red
+		color1.fill();
+
+		//get an object of Color Green
+		Color color2 = colorFactory.getColor("Green");
+
+		//call fill method of Green
+		color2.fill();
+
+		//get an object of Color Blue
+		Color color3 = colorFactory.getColor("BLUE");
+
+		//call fill method of Color Blue
+		color3.fill();
+	}
+
+}
