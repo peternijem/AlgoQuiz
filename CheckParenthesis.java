10efd8dc5ee0045a4000cb5eee1a15fdfca80851
pnijem <42067687+pnijem@users.noreply.github.com>
2018-09-09 14:19:01 +0300

    Support additional parenthesis "| |"

--- CheckParenthesis.java
+++ CheckParenthesis.java
@@ -1,55 +1,89 @@
- 
 import java.util.Stack;
+// Java program to check if all the parenthesis expression is valid.
+// Supported parenthesis: "()", "[]", "{}", "||"
 
 public class CheckParenthesis {
-	
+
 	public static boolean isValid(String str) {
-		if (str.isEmpty())
+		if (str.isEmpty()) {
 			return true;
+		}
 
 		Stack<Character> stack = new Stack<Character>();
-		for (int i = 0; i < str.length(); i++)
-		{
+		boolean toPop = false;
+		for (int i = 0; i < str.length(); i++) {
 			char current = str.charAt(i);
-			if (current == '{' || current == '(' || current == '[')
-			{
+			if (current == '{' || current == '(' || current == '[') {
 				stack.push(current);
 			}
-
-
-			if (current == '}' || current == ')' || current == ']')
-			{
-				if (stack.isEmpty())
-					return false;
-
-				char last = stack.peek();
-				if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
-					stack.pop();
-				else 
-					return false;
+			
+			if(current == '|') {
+				if(!toPop) {
+					stack.push(current);
+					toPop = true;
+					continue;
+				}
 			}
 
+			if (current == '}' || current == ')' || current == ']' ) {
+
+				if (stack.isEmpty()) {
+					return false;
+				}
+				char last = stack.peek();
+				if (current == '}' && last == '{' || 
+						current == ')' && last == '(' ||
+						current == ']' && last == '[') {
+
+					stack.pop();
+				} else {
+					return false;
+				}
+			}
+			
+			if(current == '|') {
+				if(toPop) {
+					if (stack.isEmpty()) {
+						return false;
+					}
+					char last = stack.peek();
+					if(last == '|' && current == last) {
+						stack.pop();
+						toPop = false;
+					} else {
+						return false;
+					}
+				}
+			}
 		}
 
 		return stack.isEmpty();
 	}
-	
+
 	public static void main(String arg[]){
 		String str = "((()))";
 		boolean res = CheckParenthesis.isValid(str); //true
 		System.out.println(String.format("'%s' is %s expression", str, res ? "valid" : "invalid"));
-		
+
 		str = "((())))";
 		res = CheckParenthesis.isValid(str); //false
 		System.out.println(String.format("'%s' is %s expression", str, res ? "valid" : "invalid"));
-		
+
 		str = "[((({})))]";
 		res = CheckParenthesis.isValid(str); //true
 		System.out.println(String.format("'%s' is %s expression", str, res ? "valid" : "invalid"));
-		
+
 		str = "[((({})))";
 		res = CheckParenthesis.isValid(str); //false
 		System.out.println(String.format("'%s' is %s expression", str, res ? "valid" : "invalid"));
-		
+
+		str = "|([])|";
+		res = CheckParenthesis.isValid(str); //true
+		System.out.println(String.format("'%s' is %s expression", str, res ? "valid" : "invalid"));
+
+		str = "||((({})))";
+		res = CheckParenthesis.isValid(str); //true
+		System.out.println(String.format("'%s' is %s expression", str, res ? "valid" : "invalid"));
+
 	}
 }
