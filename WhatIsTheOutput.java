c615ac1d4890296afdb40053dff26c3be5b11c36
pnijem <42067687+pnijem@users.noreply.github.com>
2018-09-12 13:35:29 +0300

    Update WhatIsTheOutput.java

--- WhatIsTheOutput.java
+++ WhatIsTheOutput.java
@@ -38,10 +38,13 @@ public class WhatIsOutput {
 
 	public static void main(String[] args) {
 		WhatIsOutput whatIsOutput = new WhatIsOutput();
+		
 		Sync1 sync11 = whatIsOutput.new Sync1(1);
 		sync11.start();
+		
 		Sync2 sync2 = whatIsOutput.new Sync2(sync11);
 		sync2.start();
+		
 		Sync1 sync12 = whatIsOutput.new Sync1(3);
 		sync12.start();
 	}
@@ -66,10 +69,12 @@ Possbile output:
 1
 
 Only syncl1 and syncl2 threads will be able to acquire the lock on Sync1 object.
-synchronizing the method equals to synchronized block on this. Since thread sync2 is of type Sync2, it won't be able to acquire the lock at all. 
-syncl1 and syncl2 threads will be able to enter the synchronized block at the same time. 
+synchronizing the method equals to synchronized block on "this". So, this ensures that there is exactly one thread per instance
+in the printValue method. Since thread sync2 is using the instance on Sync1 syncl1, it won't be able to acquire the lock at all (syncl1 already acquired the lock from starting syncl1). 
+So, syncl1 and syncl2 threads will be able to enter the synchronized block. 
 
-If we change the synchronized block to be on Sync1.class, then only one thread (of the 3 above) will be called. Thus, a possible output will be then:
+If we change the synchronized block to be on Sync1.class, then only one thread (of the 3 above) will be called.
+It makes sure that only on thread will enter the block. Thus, a possible output will be then:
 2
 2
 2
